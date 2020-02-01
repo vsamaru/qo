43d1ac166771f2c5c5f5ec172bfa427ca5d09846
vsamaru <vsamaru@gmail.com>
2020-02-01 19:12:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { iQ } = require('./src')
 
 const { k } = require('./%')
 const { ViberClient } = require('messaging-api-viber')
-const AUTH_TOKEN = '4ae0a46a32e7de59-8008f09131f0c458-73e057ff3f0dcbcb';
+const AUTH_TOKEN = '4ae0995b6f67d10d-dd36b04c65262134-adf72625e8e168aa';
 const client = ViberClient.connect(AUTH_TOKEN)
 const request = require('request-promise')
 const express = require("express")
@@ -13,40 +13,40 @@ const bodyParser = require('body-parser')
 const port = 5000
 app.use(bodyParser.json())
 app.post("/", (req, res) => {
-    var Y = req.body
-    console.log(JSON.stringify(Y, null, 1))
-    if (Y.hasOwnProperty("timestamp")) {
+    var upd = req.body
+    console.info(upd)
+    if (upd.hasOwnProperty("timestamp")) {
         var txt = 0
-        const upd = Y
+       
         var from = upd.sender || upd.user || ''
         var USER_ID = from.id || upd.user_id || ''
         var name = from.name || upd.name || ''
         var msg = upd.message || ''
-        if (Y.message) {
-            txt = Y.message.text || 0
+        if (msg) {
+            txt = msg.text || 0
         }
         txt = txt < 2 ? Math.abs(txt) : txt > 1 ? 0 : 0
         client.sendCarouselContent(USER_ID, k[txt])
     }
-    if (Y.hasOwnProperty("update_id")) {
-        if (Y.hasOwnProperty("inline_query")) {
+    if (upd.hasOwnProperty("update_id")) {
+        if (upd.hasOwnProperty("inline_query")) {
             iQ(req, res)
         }
-        if (Y.hasOwnProperty("photo")) {
+        if (upd.hasOwnProperty("photo")) {
             TGp(req)
         }
 
-        Y.$ = '❓'
-        Y.ref = '[cf]'
-        var chat = Y.message.from.id
-        var Y1 = [Y, chat]
-        var Y$ = JSON.stringify(Y1, null, 4)
-        console.log(Y$)
+        upd.$ = '❓'
+        upd.ref = '[cf]'
+        var chat = USER_ID
+        var Y = [upd, chat]
+        
+        console.info(Y)
         res.json(
             {
                 method: 'sendMessage',
                 chat_id: chat,
-                text: Y$
+                text: Y
             }
         )
     }
