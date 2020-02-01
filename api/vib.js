b6382e6846ec077d5e38065adbafdbdea41bd486
vsamaru <vsamaru@gmail.com>
2020-02-01 18:52:56 +0200

    Update vib.js

--- api/vib.js
+++ api/vib.js
@@ -4,20 +4,31 @@
  */
 const bodyParser = require('body-parser')
 const createApp = require('../common/createApp')
-const app = createApp()
-const port = 5000
-app.use(bodyParser.json())
-app.get('/api/*', (req) => {
+const app = createApp();
+const { ViberClient } = require('messaging-api-viber')
+const authToken = '4ae0a46a32e7de59-8008f09131f0c458-73e057ff3f0dcbcb'
+const client = ViberClient.connect(authToken)
+const USER_ID = 'uO2Qoyrzuxkzsp3aJAR7BA=='
+
+app.get('/api/*', (req, res) => {
   
-      return {
+
+ 
+  client.sendMessage(USER_ID, {
+    type: 'text',
+    text: 'req.body',
+    })
+    console.log(req.body)
+ 
+
+      res.json({
         success: true,
         errorCode: '200'
-        
-      } 
     })
     .catch((error) => {
-    //   console.error(error);
-      res.send(error)
+      console.log('Weather.fetchWeatherForecast ERROR', error);
+      res.send(error);
     })
-    
-exports.APP = app
+})
+
+module.exports = app;
